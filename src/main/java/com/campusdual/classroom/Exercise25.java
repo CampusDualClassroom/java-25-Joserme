090b996ef6bf86817bf00e39a36ede01e87da3c2
Joserme <jose.mendez@campusdual.com>
2024-09-28 19:43:23 +0200

    Ejercicio25 testok

--- src/main/java/com/campusdual/classroom/Exercise25.java
+++ src/main/java/com/campusdual/classroom/Exercise25.java
@@ -4,15 +4,30 @@ import java.util.Stack;
 
 public class Exercise25 {
     public static Stack<String> createStack() {
+        Stack<String> pila = new Stack<>();
+        pila.push("House");
+        pila.push("Peralta");
+        pila.push("Montessori");
+        pila.push("Smith");
 
+
+
+        return pila;
     }
 
     public static void printAndEmptyStack(Stack<String> stack) {
+        System.out.println("contenido "+ stack);
+            //stack.pop();
+        while (!stack.isEmpty()){
+            System.out.println(stack.pop());
 
+        }
     }
 
     public static void main(String[] args) {
-
+        Stack<String> pila = new Stack<>();
+        pila = createStack();
+        printAndEmptyStack(pila);
     }
 
 
